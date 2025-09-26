8730dabf069bcaaee6cd466b2a3de4867e1d5791
azar <azar@gmail.com>
2025-09-26 21:23:21 +0200

    changing hole

--- lists/tests.py
+++ lists/tests.py
@@ -5,10 +5,8 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_bad_maths(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html) 
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get('/')
+     
+        self.assertTemplateUsed(response,'index.html')
+
         
